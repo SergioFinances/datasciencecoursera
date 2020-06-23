ab1f7f98f106eacf07179764ee233825c439bffc
Sergio Andres <sergiochess95@gmail.com>
2020-06-23 13:03:40 -0500

    Funciones Quiz 2 R programming

--- Funciones Quiz 2.R
+++ Funciones Quiz 2.R
@@ -1,11 +1,14 @@
 
 #Función 1
+getwd()
+setwd("D:/documentos/MAESTRIA/datasciencecoursera/specdata")
 
 pollutantmean <- function(directory, pollutant, id=1:332) {
   files <- list.files(pattern="*.csv")
   data_list <- lapply(files, read.csv, header = TRUE)
   df <- do.call(rbind, data_list)
   df <- na.omit(df)
+  View(df)
   if (length(id) == 1){
     y <- subset(df,ID == id,select=c(pollutant))
     len <- nrow(y)
@@ -15,7 +18,7 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
   } else {
     x <- 0
     len <- 0
-    for (i in 1:id){
+    for (i in id){
       y <- subset(df,ID == i,select=c(pollutant))
       len <- len + nrow(y)
       x <- x + sum(y)
@@ -25,12 +28,9 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
   }
 }
 
-mean(df[,3])
-
 pollutantmean("specdata", "sulfate", 1:10)
 pollutantmean("specdata", "nitrate", 70:72)
 pollutantmean("specdata", "sulfate", 34)
-pollutantmean("specdata", "nitrate")
 
 # Función 2
 
@@ -51,18 +51,9 @@ complete <- function(directory,id=1:332){
   return(com)
 }
 
-complete("specdata")
-cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
-cc <- complete("specdata", 54)
-RNGversion("3.5.1")  
-set.seed(42)
-cc <- complete("specdata", 332:1)
-use <- sample(332, 10)
-print(cc[use, "nobs"])
-
+complete("specdata",c(6, 10, 20, 34, 100, 200, 310))
 
 # Función 3
-
 corr <- function(directory,threshold = 0){
   Mat <- complete(directory)
   id_2 <- data.frame()
@@ -84,26 +75,5 @@ corr <- function(directory,threshold = 0){
   return(com)
 }
 
-cr <- corr("specdata")                
-cr <- sort(cr)   
-RNGversion("3.5.1")
-set.seed(868)                
-out <- round(cr[sample(length(cr), 5)], 4)
-print(out)
+corr("specdata")                
 
-cr <- corr("specdata", 129)                
-cr <- sort(cr)                
-n <- length(cr)    
-RNGversion("3.5.1")
-set.seed(197)                
-out <- c(n, round(cr[sample(n, 5)], 4))
-print(out)
-
-cr <- corr("specdata", 2000)                
-n <- length(cr)                
-cr <- corr("specdata", 1000)                
-cr <- sort(cr)
-print(c(n, round(cr, 4)))
-
-cr <- corr("specdata", 400)
-head(cr)
